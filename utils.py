1f462d478ebacf6c2793ea3d65580b08dbf98091
Wilhan Nunes <85765131+wilhan-nunes@users.noreply.github.com>
2025-04-17 15:14:32 -0700

    update fetch_file function to include file type parameter and adjust URL construction

--- utils.py
+++ utils.py
@@ -1,31 +1,32 @@
 import requests
 import os
+from typing import Literal
 
 import streamlit as st
 
 
 @st.cache_data
-def fetch_file(task_id: str, file_name: str) -> str:
+def fetch_file(task_id: str, file_name: str, type: Literal["quant_table", "library_search_table"]) -> str:
     """
     Fetches a file from a given task ID and loads it into a pandas DataFrame.
 
     Args:
         task_id (str): The task ID to construct the file URL.
         file_name (str): The name of the file to fetch. Must be one of the predefined options.
+        type (Literal["quant_table", "library_search_table"]): The type of file to fetch.
 
     Returns:
         str: The path to the downloaded file.
     """
     # Define available options for file_name
-    valid_file_names = ["merged_results.tsv", "clustering/tall_raw_data.tsv"]
-    if file_name not in valid_file_names:
-        raise ValueError(f"Invalid file name. Choose from: {valid_file_names}")
-
-    input_url = f"https://gnps2.org/resultfile?task={task_id}&file=nf_output/{file_name}"
+    if type == "library_search_table":
+        input_url = f"https://gnps2.org/resultfile?task={task_id}&file=nf_output/{file_name}"
+    elif type == "quant_table":
+        input_url = f"https://gnps2.org/result?task={task_id}&viewname=quantificationdownload&resultdisplay_type=task"
     response = requests.get(input_url)
     response.raise_for_status()  # Raise an error for failed requests
     os.makedirs('output', exist_ok=True)
-    output_file_path = f"output/{file_name.split('/')[-1]}"
+    output_file_path = f"output/{file_name}"
 
     with open(output_file_path, 'w') as f:
         f.write(response.text)
